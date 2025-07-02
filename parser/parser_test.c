76e34c77a8d17f84e1a61d9dbdeff00200f9487f
enkoding1 <ksjdevelop53@gmail.com>
2025-07-02 15:01:40 +0900

    modify: repl 추가

--- parser/parser_test.c
+++ parser/parser_test.c
@@ -3,6 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void free_token_list(t_token_node *head)
+{
+    if (!head)
+        return ;
+    t_token_node *ptr;
+    ptr = head;
+
+    while(ptr)
+    {
+        head = ptr->next;    
+        free_token(ptr->token);
+        free(ptr);
+        ptr = head;
+    }
+}
+
 void parser_init(t_parser *parser, t_token_node *tok_head)
 {
     parser->current_token = tok_head;
@@ -145,33 +161,39 @@ void print_ast(t_node *node, int indent)
 int main(void)
 {
     
-   char *input = "< EOF cat -e | cat -e | cat -e | cat -e | cat >> outfile";
-	
-	t_lexer *lexer = new(input);
-	t_token_node *tok_head = create_token_list(lexer);
+//    char *input = "< EOF cat -e | cat -e | cat -e | cat -e | cat >> outfile";
+    // char *input = "export a= 'hello world' ";
+    char input[255];
+    while(1)
+    {
+    
+    printf(">> ");
+    fgets(input, sizeof(input), stdin);
+    if (ft_strncmp(input, "exit", 4))
+    {
+        t_lexer *lexer = new(input);
+        t_token_node *tok_head = create_token_list(lexer);
 
-	t_parser parser;
-	parser_init(&parser, tok_head);
-    print_token_list(tok_head);
-	printf("--- 파싱 시작 ---\n");
-	t_node *ast_root = parse_pipe(&parser);
+        t_parser parser;
+        parser_init(&parser, tok_head);
+        print_token_list(tok_head);
+        // printf("--- 파싱 시작 ---\n");
+        t_node *ast_root = parse_pipe(&parser);
 
-	if (parser.has_error)
-	{
-		printf("--- 문법 에러 ---\n");
-		// free_ast(ast_root);
-	}
-	else
-	{
-		printf("--- 파싱 완료 ---\n\n");
-		print_ast(ast_root, 0);
-		// free_ast(ast_root);
-
-	}
-	
-	// free_token_list(tok_head);
-	free(lexer->input);
-	free(lexer);
-
+        if (parser.has_error)
+        {
+            printf("--- 문법 에러 ---\n");
+            // free_ast(ast_root);
+        }
+        else
+        {
+            printf("--- 파싱 완료 ---\n\n");
+            print_ast(ast_root, 0);
+            // free_ast(ast_root);
+        }
+        free_token_list(tok_head);
+        free_lexer(lexer);
+    }
+}
     return 0;
 }

2abe09d8e6b3662d73f60e501441a7a4249d862f
enkoding1 <ksjdevelop53@gmail.com>
2025-07-10 02:15:51 +0900

    feat: heredoc 변수확장 기능 추가

--- executor/executor_redir.c
+++ executor/executor_redir.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include "builtin.h"
 
 static void handle_in_redir(const char *filename)
 {
@@ -41,7 +42,47 @@ static void handle_append_redir(const char *filename)
     close(fd);
 }
 
-static void handle_heredoc(const char *limiter)
+void expand_and_write_line(int fd, char *line, char **envp)
+{
+	int		i;
+	int		start;
+	char	*var_name;
+	char	*var_value;
+
+	i = 0;
+	start = 0;
+	while (line[i])
+	{
+		if (line[i] == '$')
+		{
+			if (i > start)
+				write(fd, line + start, i - start);
+
+			start = i + 1;
+			while (line[i + 1] && (isalnum(line[i + 1]) || line[i + 1] == '_'))
+				i++;
+			
+            var_name = ft_substr(line, start, i - start + 1);
+            if (!var_name) return; 
+			
+			var_value = search_envp(var_name, envp);
+			if (var_value)
+				write(fd, var_value, strlen(var_value));
+			
+			free(var_name);
+			start = i + 1;
+		}
+		i++;
+	}
+	
+	if (i > start)
+		write(fd, line + start, i - start);
+	
+	write(fd, "\n", 1);
+}
+
+
+static void handle_heredoc(const char *limiter, char **envp)
 {
     int temp_fd;
     char *line;
@@ -52,9 +93,8 @@ static void handle_heredoc(const char *limiter)
     temp_fd = open("/tmp/heredoc_tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
     
     pid = fork();
-    if (pid == 0)  // 자식 프로세스
+    if (pid == 0) // 자식 프로세스  
     {
-        // 자식 프로세스에서 heredoc 입력 처리
         close(temp_fd);
         temp_fd = open("/tmp/heredoc_tmp", O_WRONLY | O_APPEND);
         
@@ -73,9 +113,7 @@ static void handle_heredoc(const char *limiter)
                 free(line);
                 break;
             }
-            
-            ft_putstr_fd(line, temp_fd);
-            ft_putstr_fd("\n", temp_fd);
+            expand_and_write_line(temp_fd, line, envp);
             free(line);
         }
         
@@ -96,7 +134,7 @@ static void handle_heredoc(const char *limiter)
 }
 
 
-void redirection_handler(t_cmd_node *cmd_node)
+void redirection_handler(t_cmd_node *cmd_node, char **envp)
 {
     t_redir *redir = cmd_node->redirs;
     while(redir)
@@ -108,7 +146,7 @@ void redirection_handler(t_cmd_node *cmd_node)
         if (redir->type == APPEND)
             handle_append_redir(redir->filename);
         if (redir->type == HEREDOC)
-            handle_heredoc(redir->filename);
+            handle_heredoc(redir->filename, envp);
         redir = redir->next;
     }
 } 

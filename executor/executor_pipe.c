8f19d464c0714bf387d676cdbc219561cfdc49f2
enkoding1 <ksjdevelop53@gmail.com>
2025-07-14 21:17:07 +0900

    fix: 불필요한 감싸는 따옴표 제거

--- executor/executor_pipe.c
+++ executor/executor_pipe.c
@@ -4,6 +4,29 @@
 #include <unistd.h>
 #include "builtin.h"
 
+char * remove_quote(char *str)
+{
+    int position = 0;
+    int current_position = -1;
+    char quote;
+    
+    quote = str[0];
+    if (!(quote == '\"' || quote == '\''))
+        return (str);
+
+    while(str[position])
+    {
+        if(str[position] == quote)
+            current_position = position;
+        else if(str[position] == '\0')
+            break;
+        position++;
+    }
+    if (current_position == -1)
+        return (str);
+    return (ft_substr(str, 1, current_position - 1));
+}
+
 static void execute_pipe_left_child(t_pipe_node *pipe_node, char **envp, int *pipefd)
 {
     close(pipefd[0]);
@@ -14,10 +37,29 @@ static void execute_pipe_left_child(t_pipe_node *pipe_node, char **envp, int *pi
         execute_pipe((t_pipe_node *)pipe_node->left, envp);
     } else if (pipe_node->left->type == NODE_CMD)
     {
-        if (is_builtint((t_cmd_node *)pipe_node->left))
+        t_cmd_node *cmd = (t_cmd_node *)pipe_node->left;
+        int i = 0;
+        if (ft_strncmp(cmd->cmd, "echo", 4) != 0)
         {
-            redirection_handler((t_cmd_node *)pipe_node->left, envp);
-            builtin_handler((t_cmd_node *)pipe_node->left, envp);
+        while(cmd->args[i])
+        {
+            if (cmd->args[i][0] == '\"' || cmd->args[i][0] == '\'')
+            {
+                char *prev_quote = cmd->args[i];
+                char *removed_quote = remove_quote(cmd->args[i]);
+                if (prev_quote != removed_quote)
+                {
+                    free(prev_quote);
+                    cmd->args[i] = removed_quote;
+                } 
+            }
+            i++;
+        }
+        }
+        if (cmd->cmd && is_builtint(cmd))
+        {
+            redirection_handler(cmd, envp);
+            builtin_handler(cmd, envp);
             exit(0);
         }
         else
@@ -36,18 +78,29 @@ static void execute_pipe_right_child(t_pipe_node *pipe_node, char **envp, int *p
         execute_pipe((t_pipe_node *)pipe_node->right, envp);
     } else if (pipe_node->right->type == NODE_CMD)
     {
-        if (((t_cmd_node *)pipe_node)->cmd && is_builtint((t_cmd_node *)pipe_node->right))
+        t_cmd_node *cmd = (t_cmd_node *)pipe_node->right;
+        int i = 0;
+        if (ft_strncmp(cmd->cmd, "echo", 4) != 0)
         {
-            // int stdout_fd;
-            // int stdin_fd;
-            // stdout_fd = dup(STDOUT_FILENO);
-            // stdin_fd = dup(STDIN_FILENO);
-            redirection_handler((t_cmd_node *)pipe_node->right, envp);
-            builtin_handler((t_cmd_node *)pipe_node->right, envp);
-            // dup2(stdout_fd, STDOUT_FILENO);
-            // dup2(stdin_fd, STDIN_FILENO);
-            // close(stdout_fd);
-            // close(stdin_fd);
+        while(cmd->args[i])
+        {
+            if (cmd->args[i][0] == '\"' || cmd->args[i][0] == '\'')
+            {
+                char *prev_quote = cmd->args[i];
+                char *removed_quote = remove_quote(cmd->args[i]);
+                if (prev_quote != removed_quote)
+                {
+                    free(prev_quote);
+                    cmd->args[i] = removed_quote;
+                } 
+            }
+            i++;
+        }
+        }
+        if (cmd->cmd && is_builtint(cmd))
+        {
+            redirection_handler(cmd, envp);
+            builtin_handler(cmd, envp);
             exit(0);
         }
         else
@@ -90,6 +143,8 @@ void execute_pipe(t_pipe_node *pipe_node, char **envp)
     waitpid(right_pid, NULL, 0);
 }
 
+
+
 void execute(t_node *node, char **envp)
 {
     if (!node) return;
@@ -102,6 +157,25 @@ void execute(t_node *node, char **envp)
     {
         t_cmd_node *cmd = (t_cmd_node *)node;
         
+        int i = 0;
+        if (ft_strncmp(cmd->cmd, "echo", 4) != 0)
+        {
+        while(cmd->args[i])
+        {
+            if (cmd->args[i][0] == '\"' || cmd->args[i][0] == '\'')
+            {
+                char *prev_quote = cmd->args[i];
+                char *removed_quote = remove_quote(cmd->args[i]);
+                if (prev_quote != removed_quote)
+                {
+                    // free(prev_quote);
+                    cmd->args[i] = removed_quote;
+                } 
+            }
+            i++;
+        }
+        }
+
         if (cmd->cmd && is_builtint((t_cmd_node *)cmd))
         {
             int stdout_fd;

6232d75b4c16a6303156629b9f539ea7b61deeab
hamonia777 <woong010207@gmail.com>
2025-07-16 23:17:33 +0900

    refactor : paser디렉토리 norminette처리

--- parser/parse/redirection.c
+++ parser/parse/redirection.c
@@ -1,20 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirection.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinwpark <jinwpark@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 23:08:54 by jinwpark          #+#    #+#             */
+/*   Updated: 2025/07/16 23:08:55 by jinwpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parser.h"
 
-t_redir *parse_redirs(t_parser *parser)
+t_redir	*parse_redirs(t_parser *parser)
 {
-    t_redir *redir_head = (t_redir *)ft_calloc(1, sizeof(t_redir));
+	t_redir	*redir_head;
 
-    redir_head->type = peek_token(parser)->type;
-    consume_token(parser);
-
-    if (!peek_token(parser) || peek_token(parser)->type != WORD)
-    {
-        parser->has_error = 1;
-        free(redir_head);
-        return NULL; 
-    }
-
-    redir_head->filename = ft_strdup(peek_token(parser)->value);
-    consume_token(parser);
-    return redir_head;
+	redir_head = (t_redir *)ft_calloc(1, sizeof(t_redir));
+	redir_head->type = peek_token(parser)->type;
+	consume_token(parser);
+	if (!peek_token(parser) || peek_token(parser)->type != WORD)
+	{
+		parser->has_error = 1;
+		free(redir_head);
+		return (NULL);
+	}
+	redir_head->filename = ft_strdup(peek_token(parser)->value);
+	consume_token(parser);
+	return (redir_head);
 }
--- parser/parser.h
+++ parser/parser.h
@@ -1,66 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jinwpark <jinwpark@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 22:32:32 by jinwpark          #+#    #+#             */
+/*   Updated: 2025/07/16 22:32:34 by jinwpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PARSER_H
-#define PARSER_H
+# define PARSER_H
 
-#include "libft.h"
-#include "lexer.h"
+# include "lexer.h"
+# include "libft.h"
 
-typedef struct s_token_node{
-    t_token_type *token;
-    struct s_token_node *next;
-}t_token_node;
+typedef struct s_token_node
+{
+	t_token_type		*token;
+	struct s_token_node	*next;
+}						t_token_node;
 
-typedef enum e_node_type { 
-    NODE_PIPE, NODE_CMD 
-} t_node_type;
+typedef enum e_node_type
+{
+	NODE_PIPE,
+	NODE_CMD
+}						t_node_type;
 
-typedef struct s_node {
-     t_node_type type; 
-} t_node;
+typedef struct s_node
+{
+	t_node_type			type;
+}						t_node;
 
-typedef struct s_redir { 
-    t_token type; 
-    char *filename; 
-    struct s_redir *next; 
-} t_redir;
+typedef struct s_redir
+{
+	t_token				type;
+	char				*filename;
+	struct s_redir		*next;
+}						t_redir;
 
-typedef struct s_cmd_node { 
-    t_node_type type; 
-    char *cmd; 
-    char **args; 
-    t_redir *redirs; 
-} t_cmd_node;
+typedef struct s_cmd_node
+{
+	t_node_type			type;
+	char				*cmd;
+	char				**args;
+	t_redir				*redirs;
+}						t_cmd_node;
 
-typedef struct s_pipe_node { 
-    t_node_type type; 
-    t_node *left; 
-    t_node *right; 
-} t_pipe_node;
+typedef struct s_pipe_node
+{
+	t_node_type			type;
+	t_node				*left;
+	t_node				*right;
+}						t_pipe_node;
 
-typedef struct t_parser {
-    t_token_node *current_token;
-    int has_error;
-} t_parser;
+typedef struct t_parser
+{
+	t_token_node		*current_token;
+	int					has_error;
+}						t_parser;
 
 /* parser main function */
-t_node *parse_pipe(t_parser *parser);
-t_node *parse_cmd(t_parser *parser);
-t_redir *parse_redirs(t_parser *parser);
+t_node					*parse_pipe(t_parser *parser);
+t_node					*parse_cmd(t_parser *parser);
+t_redir					*parse_redirs(t_parser *parser);
 
 /* parser utils */
-void parser_init(t_parser *parser, t_token_node *tok_head);
-t_token_type * peek_token(t_parser *parser);
-void consume_token(t_parser *parser);
-void free_token_list(t_token_node *head);
-void free_ast(t_node *node);
-void free_cmd_node(t_cmd_node *cmd_node);
-void free_pipe_node(t_pipe_node *pipe_node);
-void free_redir_list(t_redir *redir);
-void print_ast(t_node *node, int indent);
+void					parser_init(t_parser *parser, t_token_node *tok_head);
+t_token_type			*peek_token(t_parser *parser);
+void					consume_token(t_parser *parser);
+void					free_token_list(t_token_node *head);
+void					free_ast(t_node *node);
+void					free_cmd_node(t_cmd_node *cmd_node);
+void					free_pipe_node(t_pipe_node *pipe_node);
+void					free_redir_list(t_redir *redir);
+void					print_ast(t_node *node, int indent);
 
 /* token list */
-t_token_node *create_token_list(t_lexer *lexer);
-t_token_node *create_token_node(t_token_type *tok);
-int add_token_node(t_token_node **head, t_token_node *new_node);
-void print_token_list(t_token_node *head);
+t_token_node			*create_token_list(t_lexer *lexer);
+t_token_node			*create_token_node(t_token_type *tok);
+int						add_token_node(t_token_node **head,
+							t_token_node *new_node);
+void					print_token_list(t_token_node *head);
 
 #endif
